1e05110bc41eb86bf63e09bd0fecbe317a2308b3
Pete Haughie <pete.haughie@sourcefabric.org>
2016-06-08 20:31:08 +0100

    Abstracted out the main styles from the reset so that portability is made easier

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function ( grunt ) {
         },
         files: {
           // compilation.css  :  source.less
+          "assets/css/reset.css": "bower_components/blankslate/reset.less",
           "assets/css/main.css": "assets/less/main.less"
         }
       },
