2f37c244d540a08105a66abc75ef7afd90150057
Pete Haughie <speedwolf@gmail.com>
2016-06-23 21:57:14 +0100

    Took away the demo functionality as it's misleading.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,19 +1,3 @@
 window.onload = function() {
 
-		var momentumElem = document.getElementById( 'momentum' );
-		var trigger = document.getElementById( 'trigger' );
-
-		var momentum = function(){
-			if ( momentumElem.classList.contains( "active" ) ) {
-				momentumElem.classList.remove( "active" );
-			} else {
-				momentumElem.classList.add( "active" );
-			}
-		};
-
-		trigger.onclick = function( event ) {
-			event.preventDefault();
-			momentum();
-		}
-
 };
