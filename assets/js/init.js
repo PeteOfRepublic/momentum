7ad265dd3e81b8508a613b0b67c1616cb730a116
Pete Haughie <speedwolf@gmail.com>
2016-06-23 22:52:22 +0100

    Added more realistic demo use case modelled on the PJazz implementation

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,46 @@
 window.onload = function() {
 
+		var momentumElem = document.getElementById( 'momentum' );
+
+		function checkState() {
+			if ( momentumElem.classList.contains( "active" ) ) {
+				momentumElem.classList.remove( "active" );
+			} else {
+				momentumElem.classList.add( "active" );
+			}
+		}
+		var style = document.createElement( 'style' );
+    document.head.appendChild( style );
+		checkState();
+
+		var i = 0;
+		var tick = 100;
+		var wait = 1500;
+
+		var loop = setInterval( function(){
+			doLoop();
+		}, tick);
+
+		function doLoop(){
+			if ( i <= 101 ) {
+				style.sheet.addRule( "#momentum.active::after", "width: " + i + "vw" );
+				i = i + 1;
+			} else {
+				i = 0;
+				pauseLoop();
+			}
+		};
+
+		function pauseLoop(){
+			clearInterval( loop );
+			checkState();
+			style.sheet.addRule( "#momentum.active::after", "width: " + 0 + "vw" );
+			setTimeout( function() {
+				checkState();
+				loop = setInterval( function(){
+					doLoop();
+				}, tick);
+			}, wait);
+		};
+
 };
